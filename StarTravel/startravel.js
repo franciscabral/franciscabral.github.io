c0e5e6db8bd46de1b42736f5c7a9e93b195cf00a
franciscabral <franciscabral@gmail.com>
2018-11-28 17:21:53 -0200

    startravel fix on widht height

--- StarTravel/startravel.js
+++ StarTravel/startravel.js
@@ -1,44 +1,45 @@
-var stars = new Array(1000)
+var stars;
 
 function setup() {
-    createCanvas(800, 800);
-    for (let i = 0; i < stars.length; i++) {
+    stars = new Array(1000);
+    createCanvas(600, 600);
+    for (var i = 0; i < stars.length; i++) {
         stars[i] = new Star();
     }
 }
 
 function draw() {
-    background(0,10);
+    background(0, 20);
 
-    translate(width/2, height/2)
-    for (let i = 0; i < stars.length; i++) {
+    translate(width / 2, height / 2);
+    for (var i = 0; i < stars.length; i++) {
         stars[i].update();
         stars[i].draw();
     }
 }
 
 function Star() {
-    this.x = random(-width, width);
-    this.y = random(-height, height);
-    this.z = random(width);//random(0,width);
+    this.x = random(-width / 2, width / 2);
+    this.y = random(-height / 2, height / 2);
+    this.z = random(0, width);
 
     this.update = function () {
-        this.z = this.z - 5;
-        if (this.z < 1){
-            this.z = width;
-            this.x = random(-width, width);
-            this.y = random(-height, height);
+        this.z = this.z - 3;
+        if (this.z < 1) {
+            this.z = random(0, width); //random(width);
+            this.x = random(-width / 2, width / 2);
+            this.y = random(-height / 2, height / 2);
         }
-        
-    }
+
+    };
 
     this.draw = function () {
         fill(255);
 
         var sx = map(this.x / this.z, 0, 1, 0, width);
         var sy = map(this.y / this.z, 0, 1, 0, height);
-
-        var r = map(this.z, 0, width, 16, 0)
+        stroke(255);
+        var r = map(this.z, 0, width, 4, 0);
         ellipse(sx, sy, r, r);
-    }
+    };
 }
